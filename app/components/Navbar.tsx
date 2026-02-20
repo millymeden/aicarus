959c757212f9b040a2eab59c452b8674e75bf4da
Milly <emily.meden@gmail.com>
2026-02-20 03:39:44 +0100

    Icn

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
           alt="Aicarus Logo"
           width={1036}
           height={704}
-          className="block h-15 w-auto translate-y-[6px]"
+          className="block h-15 w-auto translate-y-[4px]"
           priority
         />
       </Link>
